272f744d2ed41d96293f0908839536dc259cf88c
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-09-23 14:08:23 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -13,7 +13,7 @@ int sod(int n){ // sum of digits
 int main(){
     int n, a, b, sum=0;
     cin >> n >> a >> b;
-    for(int i=1; i<n; i++){
+    for(int i=1; i<=n; i++){
         if(sod(i)>=a && sod(i)<=b)  sum +=i;
     }
     cout << sum << endl;

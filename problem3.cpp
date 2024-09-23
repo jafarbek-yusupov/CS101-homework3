0318c79cfe4ec50f4e6c86dfa6c2f1593462974f
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-09-23 13:49:14 +0500

    Create problem3.cpp

---
+++ problem3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int sod(int n){ // sum of digits
+    int sum=0;
+    while(n>0){
+       sum += n%10;
+       n/=10;
+    }
+    return sum;
+}
+
+int main(){
+    int n, a, b, sum=0;
+    cin >> n >> a >> b;
+    for(int i=1; i<n; i++){
+        if(sod(i)>=a && sod(i)<=b)  sum +=i;
+    }
+    cout << sum << endl;
+    return 0;
+}
